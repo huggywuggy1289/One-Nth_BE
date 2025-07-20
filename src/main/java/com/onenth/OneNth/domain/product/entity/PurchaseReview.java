3feb97ca471c4e916242b6af58396e92bbda6959
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-07-20 17:00:45 +0900

    ✨[Feat] PurchaseReview 엔티티 reviewImages 필드 추가

--- src/main/java/com/onenth/OneNth/domain/product/entity/PurchaseReview.java
+++ src/main/java/com/onenth/OneNth/domain/product/entity/PurchaseReview.java
@@ -7,6 +7,8 @@ import lombok.*;
 import com.onenth.OneNth.domain.member.entity.Member; //+
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -32,4 +34,12 @@ public class PurchaseReview extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private Member member;
+
+    @Builder.Default
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<PurchaseReviewImage> reviewImages = new ArrayList<>();
+
+    public void addReviewImage(PurchaseReviewImage image) {
+        this.reviewImages.add(image);
+    }
 }
