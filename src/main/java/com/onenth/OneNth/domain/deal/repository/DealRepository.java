7850f76b35f80c3863038ccd59616de7285b5bdf
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-08-08 04:19:47 +0900

    🔥[Remove] Deal 엔티티 삭제

--- src/main/java/com/onenth/OneNth/domain/deal/repository/DealRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onenth.OneNth.domain.deal.repository;
-
-import com.onenth.OneNth.domain.deal.entity.Deal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DealRepository extends JpaRepository<Deal, Long> {
-}
