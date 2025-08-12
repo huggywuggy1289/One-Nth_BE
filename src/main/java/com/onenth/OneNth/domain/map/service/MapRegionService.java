8c708380bd39f0bfd85507dfac6e12cf1e60f653
galllee <kimim0410@gmail.com>
2025-08-13 04:30:25 +0900

    ✨[Feat] regionName 기반 해당 지역의 대표 좌표 반환 API 구현

--- src/main/java/com/onenth/OneNth/domain/map/service/MapRegionService.java
+++ src/main/java/com/onenth/OneNth/domain/map/service/MapRegionService.java
@@ -2,6 +2,11 @@ package com.onenth.OneNth.domain.map.service;
 
 import com.onenth.OneNth.domain.map.dto.MapResponseDTO;
 import com.onenth.OneNth.domain.region.entity.Region;
+import com.onenth.OneNth.domain.region.repository.RegionRepository;
+import com.onenth.OneNth.global.apiPayload.code.status.ErrorStatus;
+import com.onenth.OneNth.global.apiPayload.exception.GeneralException;
+import com.onenth.OneNth.global.external.kakao.dto.GeoCodingResult;
+import com.onenth.OneNth.global.external.kakao.service.GeoCodingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +14,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MapRegionService {
     private final RegionResolver regionResolver;
+    private final GeoCodingService geoCodingService;
+    private final RegionRepository regionRepository;
 
     public MapResponseDTO.GetRegionByCoordinatesResponseDTO getRegionByCoordinates(double lat, double lng) {
         Region region = regionResolver.resolveRegion(lat, lng);
@@ -16,6 +23,20 @@ public class MapRegionService {
         return MapResponseDTO.GetRegionByCoordinatesResponseDTO.builder()
                 .regionId(region.getId())
                 .regionName(region.getRegionName())
+                .build();
+    }
+
+    public MapResponseDTO.GetCoordinatesByNameResponseDTO getRegionCoordinatesByName(String regionName) {
+        Region region = regionRepository.findByRegionName(regionName)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.REGION_NOT_FOUND));
+
+        GeoCodingResult geoCodingResult = geoCodingService.getCoordinatesFromAddress(regionName);
+
+        return MapResponseDTO.GetCoordinatesByNameResponseDTO.builder()
+                .regionId(region.getId())
+                .regionName(region.getRegionName())
+                .longitude(geoCodingResult.getLongitude())
+                .latitude(geoCodingResult.getLatitude())
                 .build();
     }
 }
